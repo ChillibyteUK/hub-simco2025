35fdd49d5eb190bc1581ffd575436ad586d339ae
ChillibyteDan <dan@chillibyte.co.uk>
2025-11-06 11:54:30 +0000

    text video split

--- blocks/hub-text-video.php
+++ blocks/hub-text-video.php
@@ -8,12 +8,29 @@
 defined( 'ABSPATH' ) || exit;
 
 $level = get_field( 'level' ) ? get_field( 'level' ) : 'h2';
+
+$split = get_field( 'split' );
+
+$left = 'col-md-5';
+$right = 'col-md-7';
+
+switch ( $split ) {
+	case '50-50':
+		$left = 'col-md-6';
+		$right = 'col-md-6';
+		break;
+	case '60-40':
+		$left = 'col-md-7';
+		$right = 'col-md-5';
+		break;
+}
+
 ?>
 <section class="text-video">
 	<div class="container py-5">
 		<<?= esc_html( $level ); ?> class="<?= esc_html( $level ); ?> mb-4" data-aos="fade-right"><?= wp_kses_post( get_field( 'title' ) ); ?></<?= esc_html( $level ); ?>>
 		<div class="row g-5">
-			<div class="col-md-5">
+			<div class="<?= esc_attr( $left ); ?>">
 				<div class="text-video__content" data-aos="fade-right">
 					<?= wp_kses_post( get_field( 'content' ) ); ?>
 				</div>
@@ -26,7 +43,7 @@ $level = get_field( 'level' ) ? get_field( 'level' ) : 'h2';
 				}
 				?>
 			</div>
-			<div class="col-md-7">
+			<div class="<?= esc_attr( $right ); ?>">
 				<div class="ratio ratio-16x9">
 					<iframe src="https://player.vimeo.com/video/<?= esc_attr( get_field( 'vimeo_id' ) ); ?>"></iframe>
 				</div>
