b156ffd9f92e0acdb0d0a758d30f26c46b8078e1
ChillibyteDan <dan@chillibyte.co.uk>
2025-11-06 13:56:07 +0000

    awards

--- blocks/hub-awards.php
+++ blocks/hub-awards.php
@@ -17,17 +17,17 @@ defined( 'ABSPATH' ) || exit;
 			</div>
 			<div class="col-md-3 text-center awards__item" data-aos="fade-up" data-aos-delay="100">
 				<?= wp_get_attachment_image( get_field( 'award_logo_1' ), 'full', false, array( 'class' => 'awards__image mb-3' ) ); ?>
-				<h2 class="h3"><?= esc_html( get_field( 'award_title_1' ) ); ?></h2>
+				<h2 class="h3 fw-bold"><?= esc_html( get_field( 'award_title_1' ) ); ?></h2>
 				<div><?= wp_kses_post( get_field( 'award_detail_1' ) ); ?></div>
 			</div>
 			<div class="col-md-3 text-center awards__item" data-aos="fade-up" data-aos-delay="200">
 				<?= wp_get_attachment_image( get_field( 'award_logo_2' ), 'full', false, array( 'class' => 'awards__image mb-3' ) ); ?>
-				<h2 class="h3"><?= esc_html( get_field( 'award_title_2' ) ); ?></h2>
+				<h2 class="h3 fw-bold"><?= esc_html( get_field( 'award_title_2' ) ); ?></h2>
 				<div><?= wp_kses_post( get_field( 'award_detail_2' ) ); ?></div>
 			</div>
 			<div class="col-md-3 text-center awards__item" data-aos="fade-up" data-aos-delay="300">
 				<?= wp_get_attachment_image( get_field( 'award_logo_3' ), 'full', false, array( 'class' => 'awards__image mb-3' ) ); ?>
-				<h2 class="h3"><?= esc_html( get_field( 'award_title_3' ) ); ?></h2>
+				<h2 class="h3 fw-bold"><?= esc_html( get_field( 'award_title_3' ) ); ?></h2>
 				<div><?= wp_kses_post( get_field( 'award_detail_3' ) ); ?></div>
 			</div>
 		</div>
