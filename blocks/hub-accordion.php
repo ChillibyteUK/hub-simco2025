403ce5c75530d5f4088fa17546695902cd74ab9e
ChillibyteUK <dan@chillibyte.co.uk>
2025-11-20 16:42:27 +0000

    accordion state and hover

--- blocks/hub-accordion.php
+++ blocks/hub-accordion.php
@@ -46,6 +46,7 @@ $classes = trim( "$bg $fg $slant_class" );
 			<?php
 		}
 		$items = get_field( 'items' ) ?? array();
+
 		if ( ! empty( $items ) ) {
 			?>
 			<div class="row">
@@ -53,16 +54,17 @@ $classes = trim( "$bg $fg $slant_class" );
 		<div class="accordion" id="<?= esc_attr( $accordion_id ); ?>">
 			<?php
 			foreach ( $items as $i => $it ) {
-				$expanded = 'false';
-				$show     = '';
+				$collapsed = ( 0 === $i ) ? '' : 'collapsed';
+				$show      = ( 0 === $i ) ? 'show' : '';
+				$expanded  = ( 0 === $i ) ? 'true' : 'false';
 				?>
 			<div class="accordion-item mb-3" data-aos="fade-up" data-aos-delay="<?= esc_attr( 100 * ( $i + 1 ) ); ?>">
 				<h2 class="accordion-header" id="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
-					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" aria-expanded="<?= esc_attr( $expanded ); ?>" aria-controls="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
+					<button class="accordion-button <?= esc_attr( $collapsed ); ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" aria-expanded="<?= esc_attr( $expanded ); ?>" aria-controls="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
 						<?= esc_html( $i + 1 ); ?>. <?= esc_html( $it['title'] ?? '' ); ?>
 					</button>
 				</h2>
-				<div id="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" class="accordion-collapse collapse" aria-labelledby="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>" data-bs-parent="#<?= esc_attr( $accordion_id ); ?>">
+				<div id="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" class="accordion-collapse collapse <?= esc_attr( $show ); ?>" aria-labelledby="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>" data-bs-parent="#<?= esc_attr( $accordion_id ); ?>">
 					<div class="accordion-body">
 						<?= wp_kses_post( $it['content'] ?? '' ); ?>
 					</div>
