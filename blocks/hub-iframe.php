9ce3c623f206e5b1162dfccdb2f3b1ffa1712a49
ChillibyteUK <dan@chillibyte.co.uk>
2025-11-21 12:57:21 +0000

    defer polaris

--- blocks/hub-iframe.php
+++ blocks/hub-iframe.php
@@ -21,4 +21,17 @@ defined( 'ABSPATH' ) || exit;
 		<iframe sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="<?= esc_url( get_field( 'iframe_url' ) ); ?>" width="100%" height="725px" title="<?= esc_attr( get_field( 'title' ) ? get_field( 'title' ) : 'Embedded content' ); ?>"></iframe>
 	</div>
 </section>
-<script src="https://polaris.brighterir.com/public/sequoia_fund/chart/share_price_chart/js?scope=eod&amp;intraday_data=intraday_prices"></script>
+<?php
+add_action(
+	'wp_footer',
+	function () {
+		static $script_added = false;
+		if ( $script_added ) {
+			return;
+		}
+		$script_added = true;
+		?>
+<script src="https://polaris.brighterir.com/public/sequoia_fund/chart/share_price_chart/js?scope=eod&amp;intraday_data=intraday_prices" defer></script>
+		<?php
+	}
+);
