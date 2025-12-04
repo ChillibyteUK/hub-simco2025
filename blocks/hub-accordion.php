fc626dcb837138b93672777b2e24ba43908ce288
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 11:45:57 +0000

    accordion

--- blocks/hub-accordion.php
+++ blocks/hub-accordion.php
@@ -10,27 +10,6 @@ defined( 'ABSPATH' ) || exit;
 // Generate unique ID for this accordion instance.
 $accordion_id = 'accordion-' . uniqid();
 
-$bg = get_field( 'bg_colour' ) ? 'has-' . get_field( 'bg_colour' ) . '-background-color' : '';
-$fg = get_field( 'fg_colour' ) ? 'has-' . get_field( 'fg_colour' ) . '-color' : '';
-
-$slant_options = get_field( 'slant' ) ?? array();
-$has_top       = in_array( 'top', $slant_options, true );
-$has_bottom    = in_array( 'bottom', $slant_options, true );
-
-// Determine slant class based on selections.
-if ( $has_top && $has_bottom ) {
-	$slant_class = 'has-slanted-both-bg';
-} elseif ( $has_top ) {
-	$slant_class = 'has-slanted-top-bg';
-} elseif ( $has_bottom ) {
-	$slant_class = 'has-slanted-bg';
-} else {
-	$slant_class = 'py-5';
-}
-
-$classes = trim( "$bg $fg $slant_class" );
-
-
 ?>
 <section class="accordion-block <?= esc_attr( $classes ); ?>">
 	<div class="container" data-aos="fade-up">
@@ -49,8 +28,6 @@ $classes = trim( "$bg $fg $slant_class" );
 
 		if ( ! empty( $items ) ) {
 			?>
-			<div class="row">
-				<div class="col-md-10 offset-md-1">
 		<div class="accordion" id="<?= esc_attr( $accordion_id ); ?>">
 			<?php
 			foreach ( $items as $i => $it ) {
@@ -61,7 +38,7 @@ $classes = trim( "$bg $fg $slant_class" );
 			<div class="accordion-item mb-3" data-aos="fade-up" data-aos-delay="<?= esc_attr( 100 * ( $i + 1 ) ); ?>">
 				<h2 class="accordion-header" id="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
 					<button class="accordion-button <?= esc_attr( $collapsed ); ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" aria-expanded="<?= esc_attr( $expanded ); ?>" aria-controls="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
-						<?= esc_html( $i + 1 ); ?>. <?= esc_html( $it['title'] ?? '' ); ?>
+						<?= esc_html( $it['title'] ?? '' ); ?>
 					</button>
 				</h2>
 				<div id="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" class="accordion-collapse collapse <?= esc_attr( $show ); ?>" aria-labelledby="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>" data-bs-parent="#<?= esc_attr( $accordion_id ); ?>">
@@ -74,8 +51,6 @@ $classes = trim( "$bg $fg $slant_class" );
 			}
 			?>
 		</div>
-				</div>
-			</div>
 			<?php
 		}
 		?>
