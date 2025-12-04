a880136ddc28a83e983c07fb10e7f2d767abfc5e
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 12:25:19 +0000

    quiz

--- blocks/hub-quiz-holder.php
+++ blocks/hub-quiz-holder.php
@@ -8,10 +8,11 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<section class="quiz-holder has-lightest-grey-background-color py-5">
+<section class="quiz-holder py-5">
+	<div class="quiz-holder__overlay"></div>
 	<div class="container">
-		<h2><?= esc_html( get_field( 'title' ) ); ?></h2>
-		<div class="quiz-holder__content has-white-background-color p-4 p-md-5 mt-4">
+		<h2 class="has-h-3-font-size mb-4"><?= esc_html( get_field( 'title' ) ); ?></h2>
+		<div class="quiz-holder__content has-white-background-color p-4">
 			<?= do_shortcode( '[gravityform id="' . get_field( 'form_id' ) . '" title="false" ajax="true"]' ); ?>
 		</div>
 	</div>
