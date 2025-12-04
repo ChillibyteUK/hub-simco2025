ed3babd9b5fbfbabbe2556a4bf66b22a6381a797
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 10:52:37 +0000

    fund info

--- blocks/hub-fund-info.php
+++ blocks/hub-fund-info.php
@@ -8,14 +8,33 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div class="fund-info">
-	<div class="fund-info__title">
-		<?= wp_kses_post( get_field( 'title' ) ); ?>
+<section class="fund-info">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 has-simco-bluegrey-50-background-color has-background d-flex flex-column justify-content-center align-items-center py-5">
+				<div class="fund-info__title">
+					<?= wp_kses_post( get_field( 'title' ) ); ?>
+				</div>
+				<div class="fund-info__value">
+					<?= wp_kses_post( get_field( 'value' ) ); ?>
+				</div>
+				<div class="fund-info__after">
+					<?= wp_kses_post( get_field( 'after' ) ); ?>
+				</div>
+			</div>
+			<div class="col-md-6 p-0">
+				<?=
+				wp_get_attachment_image(
+					get_field( 'image' ),
+					'full',
+					false,
+					array(
+						'class' => 'fund-info__image',
+						'alt'   => '',
+					)
+				);
+				?>
+			</div>
+		</div>
 	</div>
-	<div class="fund-info__value">
-		<?= wp_kses_post( get_field( 'value' ) ); ?>
-	</div>
-	<div class="fund-info__after">
-		<?= wp_kses_post( get_field( 'after' ) ); ?>
-	</div>
-</div>
+</section>
