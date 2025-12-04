13cb9da6c1c81f81288a153f3cba316658363427
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 10:42:54 +0000

    icon full width py

--- blocks/hub-icon-full-width.php
+++ blocks/hub-icon-full-width.php
@@ -8,11 +8,11 @@
 defined( 'ABSPATH' ) || exit;
 
 $background = get_field( 'background' );
-$bg         = ! empty( $background ) && 'Yes' === $background[0] ? 'has-lightest-gold-background-color' : '';
+$bg         = ! empty( $background ) && 'Yes' === $background[0] ? 'has-lightest-gold-background-color py-5' : '';
 
 ?>
 <section class="full-width">
-    <div class="container <?= esc_attr( $bg ); ?> py-5" data-aos="fade-up">
+    <div class="container <?= esc_attr( $bg ); ?>" data-aos="fade-up">
 		<div class="row align-items-center">
 			<div class="col-md-2 text-center mb-4 mb-md-0">
 				<?php
