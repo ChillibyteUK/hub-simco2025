8c6dbd16b295b0282a830b8ae4e6cf87024c142a
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 09:42:34 +0000

    simcap

--- blocks/hub-simcap-contact.php
+++ blocks/hub-simcap-contact.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 	<div class="container py-5">
 		<div class="row g-5">
 			<div class="col-md-4">
-				<h2 class="h1 mb-4 text-white"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
+				<h2 class="has-h-2-font-size mb-4 text-white"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
 				<div class="simcap-contact__content mb-5"><?= wp_kses_post( get_field( 'content' ) ); ?></div>
 			</div>
 			<div class="col-md-8">
