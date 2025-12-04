417b62e58bb5849fae2de78c9430cf64d690b484
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 13:44:06 +0000

    contact

--- blocks/hub-contact.php
+++ blocks/hub-contact.php
@@ -10,9 +10,10 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="contact">
 	<div class="container py-5">
+		<h2 class="mb-4">Contact Us</h2>
 		<div class="row g-4">
-			<div class="col-md-4">
-				<h2 class="mb-4">Contact Us</h2>
+			<div class="col-md-6">
+				<iframe src="<?= esc_url( get_field( 'map_embed_code', 'option' ) ); ?>" width="100%" height="auto" style="aspect-ratio:1;border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="SIMCo Office Location Map"></iframe>
 				<div class="mb-4">
 					<div class="mb-2">SIMCo</div>
 					<?= do_shortcode( '[contact_address]' ); ?>
@@ -20,7 +21,6 @@ defined( 'ABSPATH' ) || exit;
 				<div class="mb-5">
 					<?= do_shortcode( '[contact_phone]' ); ?>
 				</div>
-				<iframe src="<?= esc_url( get_field( 'map_embed_code', 'option' ) ); ?>" width="100%" height="auto" style="aspect-ratio:1;border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="SIMCo Office Location Map"></iframe>
 			</div>
 			<div class="col-md-8">
 				<?= do_shortcode( '[gravityform id="' . get_field( 'contact_form_id' ) . '" title="false" ajax="true"]' ); ?>
