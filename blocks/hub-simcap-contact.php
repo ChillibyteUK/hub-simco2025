1ef4cfbf8ff9db7d052a329a2d4fd2e6f033e286
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 09:43:22 +0000

    simcap form

--- blocks/hub-simcap-contact.php
+++ blocks/hub-simcap-contact.php
@@ -11,11 +11,11 @@ defined( 'ABSPATH' ) || exit;
 <section class="simcap-contact">
 	<div class="container py-5">
 		<div class="row g-5">
-			<div class="col-md-4">
+			<div class="col-md-6">
 				<h2 class="has-h-2-font-size mb-4 text-white"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
 				<div class="simcap-contact__content mb-5"><?= wp_kses_post( get_field( 'content' ) ); ?></div>
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-6">
 				<?= do_shortcode( get_field( 'gf_shortcode' ) ); ?>
 			</div>
 		</div>
