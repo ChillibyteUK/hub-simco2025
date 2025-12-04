cbbaf0db83317f59e5aaa2997b02b5c7a520fcff
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-04 09:38:06 +0000

    mailing list

--- blocks/hub-mailing-list.php
+++ blocks/hub-mailing-list.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 	<div class="container py-5">
 		<div class="row g-5">
 			<div class="col-md-6">
-				<h2 class="h1 mb-4"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
+				<h2 class="has-h-2-font-size mb-4"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
 				<div class="mailing-list__content mb-5"><?= wp_kses_post( get_field( 'content' ) ); ?></div>
 			</div>
 			<div class="col-md-6">
