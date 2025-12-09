bb4ff5ef0ba8bb2c7d03bdde0e6b782e77089b49
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-09 09:15:28 +0000

    full width icon

--- blocks/hub-icon-full-width.php
+++ blocks/hub-icon-full-width.php
@@ -8,42 +8,43 @@
 defined( 'ABSPATH' ) || exit;
 
 $background = get_field( 'background' );
-$bg         = ! empty( $background ) && 'Yes' === $background[0] ? 'has-lightest-gold-background-color py-5' : '';
+$bg         = ! empty( $background ) && 'Yes' === $background[0] ? 'has-lightest-gold-background-color' : '';
 
 ?>
 <section class="full-width">
-    <div class="container <?= esc_attr( $bg ); ?>" data-aos="fade-up">
-		<div class="row align-items-center">
-			<div class="col-md-2 text-center mb-4 mb-md-0">
-				<?php
-				if ( get_field( 'icon' ) ) {
-					echo wp_get_attachment_image(
-						get_field( 'icon' ) ?? '',
-						'full',
-						false,
-						array(
-							'class' => 'full-width__icon',
-							'alt'   => esc_attr( get_field( 'title' ) . ' Icon' ),
-						)
-					);
-				}
-				?>
-			</div>
-			<div class="col-md-10">
-				<?php
-				if ( get_field( 'title' ) ) {
-					?>
-				<h2 class="has-h-3-font-size mb-4 mb-md-0"><?= esc_html( get_field( 'title' ) ); ?></h2>
+    <div class="container <?= esc_attr( $bg ); ?>">
+		<div class="py-5" data-aos="fade-up">
+			<div class="row align-items-center">
+				<div class="col-md-1 mb-4 mb-md-0">
 					<?php
-				}
-				?>
+					if ( get_field( 'icon' ) ) {
+						echo wp_get_attachment_image(
+							get_field( 'icon' ) ?? '',
+							'full',
+							false,
+							array(
+								'class' => 'full-width__icon',
+								'alt'   => esc_attr( get_field( 'title' ) . ' Icon' ),
+							)
+						);
+					}
+					?>
+				</div>
+				<div class="col-md-11">
+					<?php
+					if ( get_field( 'title' ) ) {
+						?>
+					<h2 class="has-h-3-font-size mb-4 mb-md-0"><?= esc_html( get_field( 'title' ) ); ?></h2>
+						<?php
+					}
+					?>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-10 offset-md-2">
-				<?= wp_kses_post( get_field( 'content' ) ); ?>
+			<div class="row">
+				<div class="col-md-11 offset-md-1">
+					<?= wp_kses_post( get_field( 'content' ) ); ?>
+				</div>
 			</div>
-		</div>
-		
+		</div>	
     </div>
 </section>
