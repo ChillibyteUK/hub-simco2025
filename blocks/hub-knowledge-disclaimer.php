bee0aa3b8ff9dabc26c0cf1508a5f7c6b035ab6f
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-16 13:35:03 +0000

    knowledge pushthrough

--- blocks/hub-knowledge-disclaimer.php
+++ blocks/hub-knowledge-disclaimer.php
@@ -29,8 +29,8 @@ $decline_link = get_field( 'redirect_on_decline', 'option' ) ? get_field( 'redir
                 <?= wp_kses_post( $disclaimer_content ); ?>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn--simco-red" id="decline-disclaimer-<?= esc_attr( $modal_id ); ?>">Decline</button>
-                <button type="button" class="btn btn--mid-blue" id="accept-disclaimer-<?= esc_attr( $modal_id ); ?>">Accept</button>
+                <button type="button" class="btn btn-navy" id="decline-disclaimer-<?= esc_attr( $modal_id ); ?>">Decline</button>
+                <button type="button" class="btn btn-light" id="accept-disclaimer-<?= esc_attr( $modal_id ); ?>">Accept</button>
             </div>
         </div>
     </div>
--- blocks/hub-knowledge-pushthrough.php
+++ blocks/hub-knowledge-pushthrough.php
@@ -7,33 +7,17 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$col_order   = get_field( 'order' );
-$image_order = 'order-lg-2';
-$text_order  = 'order-lg-1';
-
-$text_alignment = 'text--left ';
-
-$img = get_field( 'image' ) ?
-	wp_get_attachment_image(
-		get_field( 'image' ),
-		'full',
-		false,
-		array( 'alt' => esc_attr( get_field( 'title' ) ) )
-	) :
-	'<img src="' . get_stylesheet_directory_uri() . '/img/placeholder-800x450.png" alt="Placeholder image">';
 ?>
-<section class="split <?= esc_attr( $classes ); ?> has-simco-gold-background-color has-background-color">
-    <div class="container-fluid">
-        <div class="h2 d-lg-none text-center pt-3">
-            <?= esc_html( get_field( 'title' ) ); ?>
-        </div>
-        <div class="row h-100">
-            <div class="col-lg-6 split__image <?= esc_attr( $image_order ); ?>">
-                <div class="split__image-wrapper" data-aos="<?= esc_attr( $image_aos ); ?>">
-                    <?= wp_kses_post( $img ); ?>
-                </div>
-            </div>
-            <div class="col-lg-6 h-100 my-auto <?= esc_attr( $text_order ); ?>">
+<section class="knowledge-pushthrough">
+
+	<div class="knowledge-pushthrough__background">
+		<?= wp_get_attachment_image( get_field( 'image' ), 'full', false, array( 'alt' => '' ) ); ?>
+	</div>
+	<div class="knowledge-pushthrough__overlay" aria-hidden="true"></div>
+
+	<div class="container py-5">
+		<div class="row align-items-center">
+            <div class="col-lg-6 h-100 my-auto">
                 <div class="ps-xl-3 py-5 my-auto <?= esc_attr( $text_alignment ); ?>" data-aos="<?= esc_attr( $text_aos ); ?>">
                     <h2 class="h2 d-none d-lg-block"><?= esc_html( get_field( 'title' ) ); ?></h2>
 					<?= wp_kses_post( get_field( 'content' ) ); ?>
@@ -42,9 +26,11 @@ $img = get_field( 'image' ) ?
 						$l      = get_field( 'button' );
 						$target = $l['target'] ? $l['target'] : '_self';
                         ?>
+					<div>
 					<a href="<?= esc_url( $l['url'] ); ?>"
 						class="btn btn-light mx-auto mt-4 ms-md-0"
 						target="<?= esc_attr( $target ); ?>"><?= esc_html( $l['title'] ); ?></a>
+					</div>
 						<?php
 					}
 					?>
