dc6b1a33caa272c4809dda1ac6013d4e90d09193
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-09 09:40:42 +0000

    hp hero

--- blocks/hub-homepage-hero.php
+++ blocks/hub-homepage-hero.php
@@ -15,8 +15,8 @@ defined( 'ABSPATH' ) || exit;
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-md-7">
-				<h1 class="has-simco-gold-color mb-5"><?= wp_kses_post( get_field( 'title' ) ); ?></h1>
-				<a href="/insights/" class="btn btn-light">Insights</a>
+				<div class="has-h-2-font-size">Sequoia Investment Management Company</div>
+				<h1 class="mb-5"><?= wp_kses_post( get_field( 'title' ) ); ?></h1>
 			</div>
 		</div>
 	</div>
