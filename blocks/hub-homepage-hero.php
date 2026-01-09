b6d24d662093e095f6600b31ee7bedffc33ba8fa
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-09 09:50:12 +0000

    hp hero

--- blocks/hub-homepage-hero.php
+++ blocks/hub-homepage-hero.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-md-7">
-				<div class="has-h-2-font-size">Sequoia Investment Management Company</div>
+				<div class="has-h-3-font-size">Sequoia Investment Management Company</div>
 				<h1 class="mb-5"><?= wp_kses_post( get_field( 'title' ) ); ?></h1>
 			</div>
 		</div>
