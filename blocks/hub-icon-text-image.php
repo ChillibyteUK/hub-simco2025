d609fcb5f5f08ca7fc25c2faac3938a9819be259
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-09 10:55:28 +0000

    icon text image

--- blocks/hub-icon-text-image.php
+++ blocks/hub-icon-text-image.php
@@ -30,7 +30,7 @@ if ( $block_id ) {
 		<div class="row">
 			<div class="col-lg-6 p-5 <?= esc_attr( $text_order ); ?>"">
 				<div class="" data-aos="fade-up">
-					<div class="d-flex gap-3 align-items-center">
+					<div class="mb-4 d-flex gap-3 align-items-center">
 					<?php
 					if ( get_field( 'icon' ) ) {
 						echo wp_get_attachment_image(
@@ -45,7 +45,7 @@ if ( $block_id ) {
 					}
 					if ( get_field( 'title' ) ) {
 						?>
-					<h2 class="mt-3 has-h-3-font-size"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
+					<h2 class="has-h-3-font-size"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
 						<?php
 					}
 					?>
