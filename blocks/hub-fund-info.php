771e84269fd10cec1c5f047362cb8ca93ac944e4
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-09 11:45:33 +0000

    fund info

--- blocks/hub-fund-info.php
+++ blocks/hub-fund-info.php
@@ -9,32 +9,15 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <section class="fund-info">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 has-simco-bluegrey-50-background-color has-background d-flex flex-column justify-content-center align-items-center py-5">
-				<div class="fund-info__title">
-					<?= wp_kses_post( get_field( 'title' ) ); ?>
-				</div>
-				<div class="fund-info__value">
-					<?= wp_kses_post( get_field( 'value' ) ); ?>
-				</div>
-				<div class="fund-info__after">
-					<?= wp_kses_post( get_field( 'after' ) ); ?>
-				</div>
-			</div>
-			<div class="col-md-6 p-0">
-				<?=
-				wp_get_attachment_image(
-					get_field( 'image' ),
-					'full',
-					false,
-					array(
-						'class' => 'fund-info__image',
-						'alt'   => '',
-					)
-				);
-				?>
-			</div>
+	<div class="has-simco-bluegrey-50-background-color has-background d-flex flex-column justify-content-center align-items-center py-5">
+		<div class="fund-info__title">
+			<?= wp_kses_post( get_field( 'title' ) ); ?>
+		</div>
+		<div class="fund-info__value">
+			<?= wp_kses_post( get_field( 'value' ) ); ?>
+		</div>
+		<div class="fund-info__after">
+			<?= wp_kses_post( get_field( 'after' ) ); ?>
 		</div>
 	</div>
 </section>
